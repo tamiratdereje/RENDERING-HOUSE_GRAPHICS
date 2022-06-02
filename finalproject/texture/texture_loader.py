ec1f3bb50011946d0f9593f5cbe3781b62264e2a
t5691yifru@gmail.com <t5691yifru@gmail.com>
2022-06-02 21:02:33 +0300

    done with texture

--- finalproject/texture/texture_loader.py
+++ finalproject/texture/texture_loader.py
@@ -4,3 +4,17 @@ from OpenGL.GL import glBindTexture, glTexParameteri, GL_TEXTURE_2D, GL_TEXTURE_
 
 from PIL import Image
 
+
+def load_texture(path, texture):
+
+    glBindTexture(GL_TEXTURE_2D, texture)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+    image = Image.open(path)
+
+    image = image.transpose(Image.FLIP_TOP_BOTTOM)
+    img_data = image.convert("RGBA").tobytes()
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width, image.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img_data)
+    return texture
